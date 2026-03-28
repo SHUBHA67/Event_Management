41d25304e45edd58ae421f8c1286f78fb3f7db6e
sangitaaaaa <dummy@gmail.com>
2026-03-28 04:12:49 +0000

    allocation entity by sangita

--- server/src/main/java/com/edutech/eventmanagementsystem/entity/Allocation.java
+++ server/src/main/java/com/edutech/eventmanagementsystem/entity/Allocation.java
@@ -1,12 +1,42 @@
 package com.edutech.eventmanagementsystem.entity;
 
-
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.persistence.*;
-
 
 @Table(name = "allocations") // do not change table name
 public class Allocation {
     // implement entity
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long allocationID;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    @JsonBackReference
+    private Event event;
+
+    @ManyToOne
+    @JoinColumn(name = "resource_id")
+    private Resource resource;
+
+    private int quantity;
+
+    public Long getAllocationID() {
+        return allocationID;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public Resource getResource() {
+        return resource;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    
 }
--- server/src/main/java/com/edutech/eventmanagementsystem/repository/AllocationRepository.java
+++ server/src/main/java/com/edutech/eventmanagementsystem/repository/AllocationRepository.java
@@ -1,12 +1,10 @@
 package com.edutech.eventmanagementsystem.repository;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.edutech.eventmanagementsystem.entity.Allocation;
 
-
-public interface AllocationRepository  {
+public interface AllocationRepository extends JpaRepository {
     // extend jpa repository and add custom methods if needed
 }
