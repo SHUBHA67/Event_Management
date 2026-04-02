63b81028fe691b9bdbca20dc9d08b34baa16b27f
SHUBHA67 <dummy@gmail.com>
2026-04-02 16:02:51 +0000

    commit by shubham

--- client/src/app/booking-details/booking-details.component.ts
+++ client/src/app/booking-details/booking-details.component.ts
@@ -9,11 +9,33 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './booking-details.component.html',
   styleUrls: ['./booking-details.component.scss']
 })
-export class BookingDetailsComponent {
+export class BookingDetailsComponent implements OnInit {
 
+  bookings: any[] = [];
+  showError = false;
+  errorMessage = '';
+
+  constructor(private httpService: HttpService) { }
+
+  ngOnInit(): void {
+    this.loadMyBookings();
+  }
+
+  loadMyBookings(): void {
+    this.httpService.getMyBookings().subscribe({
+      next: (res: any) => {
+        this.bookings = res;
+        this.showError = false;
+      },
+      error: () => {
+        this.showError = true;
+        this.errorMessage = 'Failed to load your booking details.';
+      }
+    });
+  }
 }
 
 ///todo: complete missing code.
 
-  
+
 
